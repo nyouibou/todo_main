efe664cd294e029c1892ead919cf4a253e7bad8a
akshai <57423537+nyouibou@users.noreply.github.com>
2024-03-10 15:08:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/view/splash_screen/splash_screen.dart';
+
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter;
+  var box = await Hive.openBox('notebox');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/view/home_page/widgets/customradiobuttons.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/utils/color_constants/color_constants.dart';
+
+class custonotewidget extends StatefulWidget {
+  const custonotewidget({
+    Key? key,
+    required this.title,
+    required this.des,
+    this.ondeletepressed,
+    this.oneditpressed,
+  }) : super(key: key);
+
+  final String title;
+  final String des;
+  final VoidCallback? ondeletepressed;
+  final VoidCallback? oneditpressed;
+
+  @override
+  State<custonotewidget> createState() => _custonotewidgetState();
+}
+
+class _custonotewidgetState extends State<custonotewidget> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  // Add a Radio button
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isSelected = !isSelected;
+                      });
+                    },
+                    child: Radio<bool>(
+                      value: true,
+                      groupValue: isSelected,
+                      onChanged: (value) {
+                        setState(() {
+                          isSelected = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width *
+                        0.5, // Adjust the width as needed
+                    child: Text(
+                      widget.title,
+                      style: TextStyle(
+                          color: Colorconstants.mainblack,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.edit,
+                      color: Colorconstants.mainblack,
+                    ),
+                    onPressed: () {
+                      widget.oneditpressed!();
+                    },
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colorconstants.mainblack,
+                    ),
+                    onPressed: () {
+                      widget.ondeletepressed!();
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width *
+                0.5, // Adjust the width as needed
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  widget.des,
+                  style: TextStyle(color: Colorconstants.mainblack),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
